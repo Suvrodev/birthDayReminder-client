0825510298362bfc662e6c3ed034d4bb8eba2aa5
Suvrodev Howlader <suvrodev.cse@yahoo.com>
2023-05-12 07:37:37 +0600

    Added Route

---
+++ src/Route/route.jsx
@@ -0,0 +1,14 @@
+import {
+    createBrowserRouter,
+    RouterProvider,
+  } from "react-router-dom";
+
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <div>Hello world!</div>,
+    },
+  ]);
+
+  export default router;
